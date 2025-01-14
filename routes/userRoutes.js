91a4a2155838dde124f8d472370239175979e232
xiaowu <2728795232@qq.com>
2025-01-14 21:11:25 +0800

    feat(userController): extract user-related functions to userController - MVC Model

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,42 +1,13 @@
 const express = require('express')
+const {
+    getAllUsers,
+    createUser,
+    getUser,
+    updateUser,
+    deleteUser,
+} = require('../controllers/userController')
 
 const userRouter = express.Router()
-
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const getUser = (req, res) => {
-    // 500: server error
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
 
 userRouter.route('/').get(getAllUsers).post(createUser)
 
