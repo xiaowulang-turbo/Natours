9ff37e2723315696baf5b2e770e73c0646b8008a
xiaowu <2728795232@qq.com>
2025-01-14 21:02:06 +0800

    refactor: Extract tour and user routes to separate modules

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const fs = require('fs')
 const morgan = require('morgan')
 
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+
 const app = express()
 
 // 1) MIDDLEWARE
@@ -22,164 +25,9 @@ app.use((req, res, next) => {
     next()
 })
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
-// console.log(tours);
-
-// 2) ROUTE HANDLERS
-
-const getAllTours = (req, res) => {
-    console.log(req.requestTime)
-
-    res.status(200).json({
-        status: 'success',
-        // only count when we have an array
-        requestedAt: req.requestTime,
-        results: tours?.length || 1,
-        data: {
-            tours,
-        },
-    })
-}
-
-const getTour = (req, res) => {
-    console.log(req.params)
-
-    // a nice trick to convert string to number
-    const id = req.params.id * 1
-    const tour = tours.find((el) => el.id === id)
-
-    // variables made by const and let cannot be accessed before where they are declared
-    // if (id > tours.length) {
-    if (!tour) {
-        // use return to stop the function immediately
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        })
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour,
-        },
-    })
-}
-
-const createTour = (req, res) => {
-    //console.log(req.body);
-    const id = tours[tours.length - 1].id + 1
-
-    const newTour = Object.assign({ id }, req.body)
-    tours.push(newTour)
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        (err) => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour,
-                },
-            })
-        }
-    )
-}
-
-const updateTour = (req, res) => {
-    if (req.params.id * 1 >= tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        })
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>',
-        },
-    })
-}
-
-const deleteTour = (req, res) => {
-    if (req.params.id * 1 >= tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID',
-        })
-    }
-
-    // 204: no content
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    })
-}
-
-//
-
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const getUser = (req, res) => {
-    // 500: server error
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
-
-/*
 // Only the callback function is running in event loop
-app.get('/api/v1/tours', getAllTours);
-// The route should be matched exactly, use ? to make some params optional
-app.get('/api/v1/tours/:id/:x/:y?', getTour);
-app.post('/api/v1/tours', createTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
-*/
 
 // 3) ROUTES
-
-const tourRouter = express.Router()
-const userRouter = express.Router()
-
-tourRouter.route('/').get(getAllTours).post(createTour)
-
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
-
-userRouter.route('/').get(getAllUsers).post(createUser)
-
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
 
 // Mounting a new router on a route
 app.use('/api/v1/tours', tourRouter)
