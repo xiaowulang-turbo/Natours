e30bf0e16e01e1d0af09341cac3f01b48d9152b0
xiaowu <2728795232@qq.com>
2025-01-17 19:56:11 +0800

    feat: add test tour creation and save functionality

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ dotenv.config({
     path: './config.env',
 })
 
-mongoose
-    .connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
-    .then((con) => console.log('DB connection successful!', con.connection))
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
 
 // This env is set by express
 // console.log(app.get('env'))
@@ -21,6 +19,24 @@ mongoose
 // Everything has nothing to do with express should be done outside app.js
 // console.log(process.env)
 // this process is set by node.js
+
+const tourSchema = require('./models/tourModel')
+
+const testTour = new tourSchema({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 497,
+})
+console.log(testTour)
+
+testTour
+    .save()
+    .then((doc) => {
+        console.log(doc)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 const port = 3000
 app.listen(port, () => {
