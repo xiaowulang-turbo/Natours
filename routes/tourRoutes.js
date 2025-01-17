51eab7c0d45b296aac6c6e81e6608e6fe3bbd62d
xiaowu <2728795232@qq.com>
2025-01-17 21:01:13 +0800

    refactor(tourRoutes): Remove checkBody middleware from createTour route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,14 +11,14 @@ const {
 
 const router = express.Router()
 
-router.param('id', checkID)
+// router.param('id', checkID)
 
 // Create a checkBody middleware
 // Check if the body contains the name and price property
 // If not, send back 400 (Bad Request)
 // Add it to the post handler stack
 
-router.route('/').get(getAllTours).post(checkBody, createTour)
+router.route('/').get(getAllTours).post(createTour)
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
