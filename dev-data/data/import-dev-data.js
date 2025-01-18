d4993ea8eb1fb9efdadecd8d31348fefcf8c1750
xiaowu <2728795232@qq.com>
2025-01-18 16:39:31 +0800

    feat: Add import and delete functionality for tours data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,3 +2,45 @@ const fs = require('fs')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const Tour = require('../../models/tourModel')
+
+dotenv.config({ path: './config.env' })
+
+// The mongondb create method needs a js object rather than a json file
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8', () => {
+        console.log('data loaded')
+    })
+)
+
+// console.log(tours)
+
+mongoose
+    .connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('DB connection successful')
+    })
+
+const method = process.argv[2]
+
+if (method === '--import') {
+    Tour.create(tours)
+        .then((docs) => {
+            console.log(`${docs.length} tours added successfully`)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+} else if (method === '--delete') {
+    Tour.deleteMany()
+        .then(() => {
+            console.log('data deleted')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
