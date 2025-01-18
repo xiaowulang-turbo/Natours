c6128144a0166e016fa549f6147b6d0e899d8c1d
xiaowu <2728795232@qq.com>
2025-01-18 18:12:54 +0800

    feat(tourController): enhance getAllTours with query filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,7 +39,21 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find()
+        const queryObj = { ...req.query }
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
+        excludedFields.forEach((el) => delete queryObj[el])
+
+        // we cannot use await here because we need to deal with other queries like sort, limit, page later
+        const query = Tour.find(queryObj)
+
+        // const query = Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy')
+
+        const tours = await query
+
         res.status(200).json({
             status: 'success',
             // only count when we have an array
