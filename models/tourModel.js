92ad61f81d8771a261b7be061a5d7428c8379fbb
xiaowu <2728795232@qq.com>
2025-01-18 16:39:25 +0800

    feat(tourModel): add new fields and update schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,56 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
     },
-    price: {
+    duration: {
         type: Number,
-        required: [true, 'A tour must have a price'],
+        required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        //Validators
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
     },
     rating: {
         type: Number,
         default: 4.5,
         //Validators
     },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true, // Only work for strings, removes white spaces from the beginning and end
+        required: [true, 'A tour must have a summary'],
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date],
 })
 
 module.exports = mongoose.model('Tour', tourSchema)
