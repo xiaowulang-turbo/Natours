8cd0b8fddc482ab1b59d9b2f386ceea034424e71
xiaowu <2728795232@qq.com>
2025-01-19 12:34:41 +0800

    refactor(tourController): simplify getAllTours with APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -103,61 +103,6 @@ class APIFeatures {
 
 exports.getAllTours = async (req, res) => {
     try {
-        console.log(req.query)
-        // BUILD QUERY
-        /*
-        // 1A) Filtering
-
-        const queryObj = { ...req.query }
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach((el) => delete queryObj[el])
-
-        // 1B) Advanced filtering
-        // we cannot use await here because we need to deal with other queries like sort, limit, page later
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(
-            /\b(gte|gt|lte|lt)\b/g,
-            (match) => `$${match}`
-        )
-        let query = Tour.find(JSON.parse(queryStr))
-
-        // const query = Tour.find()
-        //     .where('duration')
-        //     .equals(5)
-        //     .where('difficulty')
-        //     .equals('easy')
-        
-        // 2) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ')
-            // sortBy is a string rather than an array
-            query = query.sort(sortBy)
-        } else {
-            query = query.sort('-createdAt')
-        }
-
-        // 3) Field limiting, called projecting as well
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v')
-        }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1
-        const limit = req.query.limit * 1 || 100
-        const skip = (page - 1) * limit
-
-        query = query.skip(skip).limit(limit)
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments()
-            if (skip >= numTours) {
-                throw new Error('This page does not exist')
-            }
-        }
-        */
         // EXECUTE QUERY
         const features = new APIFeatures(Tour.find(), req.query)
             .filter()
