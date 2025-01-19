09264cbb54313ec9aade21870d5b07c73c469d3d
xiaowu <2728795232@qq.com>
2025-01-19 20:06:26 +0800

    feat(tourRoutes): add monthly plan route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
     deleteTour,
     aliasTopTours,
     getTourStats,
+    getMonthlyPlan,
 } = require('../controllers/tourController')
 
 const router = express.Router()
@@ -22,6 +23,8 @@ const router = express.Router()
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
 
 router.route('/tour-stats').get(getTourStats)
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 router.route('/').get(getAllTours).post(createTour)
 
