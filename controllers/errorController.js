c2ec5b5ca7256d99269bdb185d13369d825a2914
xiaowu <2728795232@qq.com>
2025-01-23 13:01:06 +0800

    refactor(errorController): improve error handling by separating development and production responses

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,41 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    })
+}
+
+const sendErrorProd = (err, res) => {
+    // All errors that we create are operational errors
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+        // Programming or other unknown error: don't leak error details
+    } else {
+        // 1) Log error
+        console.error('ERROR 💥', err)
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong',
+        })
+    }
+}
+
 module.exports = (err, req, res, next) => {
     // console.log(err.stack)
 
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    })
-    next()
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res)
+    }
 }
