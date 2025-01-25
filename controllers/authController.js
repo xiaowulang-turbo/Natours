06465faa5bc21442ed78c828d64a725db2e0f8ed
xiaowu <2728795232@qq.com>
2025-01-25 15:55:35 +0800

    feat(auth): implement user signup controller with secure field selection

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const User = require('../models/userModel')
+const catchAsync = require('../utils/catchAsync')
+
+exports.signup = catchAsync(async (req, res, next) => {
+    // Specify the fields that are required are safer than using req.body, since req.body can be manipulated and some users can be created as a admin easily
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    })
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser,
+        },
+    })
+})
