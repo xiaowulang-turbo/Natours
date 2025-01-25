a441e5b5dba556f7319a11154543444746ad1d04
xiaowu <2728795232@qq.com>
2025-01-25 15:55:44 +0800

    feat(routes): add signup route for user registration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,8 +6,12 @@ const {
     updateUser,
     deleteUser,
 } = require('../controllers/userController')
+const { signup } = require('../controllers/authController')
 
 const router = express.Router()
+
+// this route is not a good restful api practice, but it is a good way to sign up a user
+router.post('/signup', signup)
 
 router.route('/').get(getAllUsers).post(createUser)
 
