adf76cd2a9933f64834038056df943f9df1b0a1f
xiaowu <2728795232@qq.com>
2025-01-25 21:30:08 +0800

    feat(auth): implement user login with JWT token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 exports.signup = catchAsync(async (req, res, next) => {
     // Specify the fields that are required are safer than using req.body, since req.body can be manipulated and some users can be created as a admin easily
@@ -27,5 +28,41 @@ exports.signup = catchAsync(async (req, res, next) => {
         data: {
             user: newUser,
         },
+    })
+})
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400))
+    }
+
+    // 2) Check if user exists and password is correct
+    const user = await User.findOne({ email }).select('+password')
+    // We cannot use the sentence below since the user might not exist
+    // const correct = await user.correctPassword(password, user.password)
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        // The error message is a little bit vague so it's good for security
+        return next(new AppError('Incorrect email or password', 401))
+        // 401: unauthorized
+    }
+
+    // 3) If everything ok, send token to client
+    const token = jwt.sign(
+        {
+            id: user._id,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        }
+    )
+
+    res.status(200).json({
+        status: 'success',
+        token,
     })
 })
