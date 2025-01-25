b5942bfa6d76b7751ac6640948fd90946b543aac
xiaowu <2728795232@qq.com>
2025-01-25 20:30:36 +0800

    feat(auth): generate JWT token upon user signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
+const jwt = require('jsonwebtoken')
 
 exports.signup = catchAsync(async (req, res, next) => {
     // Specify the fields that are required are safer than using req.body, since req.body can be manipulated and some users can be created as a admin easily
@@ -10,8 +11,19 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm,
     })
 
+    const token = jwt.sign(
+        {
+            id: newUser._id,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        }
+    )
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser,
         },
