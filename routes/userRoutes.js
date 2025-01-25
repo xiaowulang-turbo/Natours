98e32ab92e9928ece79cde0b47b86c7f6176113c
xiaowu <2728795232@qq.com>
2025-01-25 21:39:09 +0800

    feat(routes): add login route to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,14 @@ const {
     updateUser,
     deleteUser,
 } = require('../controllers/userController')
-const { signup } = require('../controllers/authController')
+const { signup, login } = require('../controllers/authController')
 
 const router = express.Router()
 
 // this route is not a good restful api practice, but it is a good way to sign up a user
 router.post('/signup', signup)
+
+router.post('/login', login)
 
 router.route('/').get(getAllUsers).post(createUser)
 
