14acd15d162da9480e1bf4f35c23f87e2b205f65
xiaowu <2728795232@qq.com>
2025-01-25 14:27:51 +0800

    feat(models): create user model with validation and role-based access

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email!'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email!'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password!'],
+        minlength: 8,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password!'],
+        validate: {
+            validator: function (el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same!',
+        },
+    },
+    photo: String,
+    role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user',
+    },
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
