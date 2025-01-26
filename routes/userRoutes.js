303a31dfc59ce35f4eb6a6c5de72312ee9844d7b
xiaowu <2728795232@qq.com>
2025-01-26 21:19:08 +0800

    feat(auth): add forgot password route for user password reset

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,15 +11,15 @@ const {
     login,
     protect,
     restrictTo,
+    forgotPassword,
 } = require('../controllers/authController')
 
 const router = express.Router()
 
 // this route is not a good restful api practice, but it is a good way to sign up a user
 router.post('/signup', signup)
-
 router.post('/login', login)
-
+router.post('/forgotPassword', forgotPassword)
 router.route('/').get(protect, getAllUsers).post(createUser)
 
 router
