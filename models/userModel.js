29046e91ce51eef13cf1902be76d10ebadb76631
xiaowu <2728795232@qq.com>
2025-01-26 21:19:22 +0800

    feat(auth): implement createPasswordResetToken method for user password reset

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,6 +39,8 @@ const userSchema = new mongoose.Schema({
         enum: ['user', 'guide', 'lead-guide', 'admin'],
         default: 'user',
     },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
 })
 
 // It is a good idea to use pre middleware to hash password
@@ -72,6 +75,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     }
 
     return false
+}
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+    console.log({ resetToken }, { passwordResetToken: this.passwordResetToken })
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+
+    return resetToken
 }
 
 const User = mongoose.model('User', userSchema)
