23ca73a8130aed8737a48e7c5826ed94704970a2
xiaowu <2728795232@qq.com>
2025-01-27 20:41:23 +0800

    feat(auth): add updatePassword controller method for user password changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -197,3 +197,29 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
         token,
     })
 })
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+    // 1) Get user from collection
+    const user = await User.findById(req.user.id).select('+password')
+
+    // 2) Check if POSTed current password is correct
+    if (
+        !user ||
+        !(await user.correctPassword(req.body.passwordCurrent, user.password))
+    ) {
+        return next(new AppError('Incorrect password', 401))
+    }
+
+    // 3) If so, update password
+    user.password = req.body.password
+    user.passwordConfirm = req.body.passwordConfirm
+    await user.save()
+
+    // 4) Log user in, send JWT
+    const token = signToken(user._id)
+
+    res.status(200).json({
+        status: 'success',
+        token,
+    })
+})
