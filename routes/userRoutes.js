d6381a4b0762ed5a8884e1f9aac93ea81800d94f
xiaowu <2728795232@qq.com>
2025-01-27 12:18:52 +0800

    feat(routes): add route for password reset endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,7 @@ const {
     protect,
     restrictTo,
     forgotPassword,
+    resetPassword,
 } = require('../controllers/authController')
 
 const router = express.Router()
@@ -20,6 +21,7 @@ const router = express.Router()
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/forgotPassword', forgotPassword)
+router.patch('/resetPassword/:token', resetPassword)
 router.route('/').get(protect, getAllUsers).post(createUser)
 
 router
