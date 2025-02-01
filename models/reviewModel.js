9fc050c2c2c6d938236fb132451f3030596a58a3
xiaowu <2728795232@qq.com>
2025-02-01 17:56:58 +0800

    feat: create review model with tour and user references

---
+++ models/reviewModel.js
@@ -0,0 +1,49 @@
+// review / rating / createdAt / tour / user
+const mongoose = require('mongoose')
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review cannot be empty'],
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: [true, 'Rating cannot be empty'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour'],
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user'],
+        },
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+)
+
+// 查询中间件
+// reviewSchema.pre(/^find/, function (next) {
+//     this.populate({
+//         path: 'user',
+//         select: 'name photo',
+//     })
+
+//     next()
+// })
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = Review
