3f01441b7a075232f5b5c40e8819e43e4f80bc67
xiaowu <2728795232@qq.com>
2025-02-02 12:25:04 +0800

    feat: Implement generic delete handler for user deletion

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel')
 const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
+const { deleteOne } = require('./handlerFactory')
 
 exports.getAllUsers = catchAsync(async (req, res) => {
     const users = await User.find()
@@ -86,9 +87,11 @@ exports.updateUser = (req, res) => {
     })
 }
 
-exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined',
-    })
-}
+// exports.deleteUser = (req, res) => {
+//     res.status(500).json({
+//         status: 'error',
+//         message: 'This route is not yet defined',
+//     })
+// }
+
+exports.deleteUser = deleteOne(User)
