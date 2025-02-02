ffccf8b2f23709946f9a87df0bca3a639066ba0a
xiaowu <2728795232@qq.com>
2025-02-02 13:26:20 +0800

    refactor: Simplify review creation using generic handler and add middleware for setting tour and user IDs

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,6 @@
 const Review = require('../models/reviewModel')
 const catchAsync = require('../utils/catchAsync')
-const { deleteOne, updateOne } = require('./handlerFactory')
+const { deleteOne, updateOne, createOne } = require('./handlerFactory')
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
     let filter = {}
@@ -15,17 +15,12 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     })
 })
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
     if (!req.body.tour) req.body.tour = req.params.tourId
     if (!req.body.user) req.body.user = req.user.id
+    next()
+}
 
-    const review = await Review.create(req.body)
-
-    res.status(201).json({
-        status: 'success',
-        data: { review },
-    })
-})
-
+exports.createReview = createOne(Review)
 exports.updateReview = updateOne(Review)
 exports.deleteReview = deleteOne(Review)
