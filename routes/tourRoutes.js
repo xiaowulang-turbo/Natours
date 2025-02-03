50d736bd1ababd7ad2ed52c6fdcaa8c4594a5094
xiaowu <2728795232@qq.com>
2025-02-03 21:18:08 +0800

    feat: Add route for calculating distances from a point

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
     getTourStats,
     getMonthlyPlan,
     getToursWithin,
+    getDistances,
 } = require('../controllers/tourController')
 const { protect, restrictTo } = require('../controllers/authController')
 const reviewRouter = require('./reviewRoutes')
@@ -38,6 +39,8 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router
     .route('/tours-within/:distance/center/:latlng/unit/:unit')
     .get(getToursWithin)
+
+router.route('/distances/:latlng/unit/:unit').get(getDistances)
 
 router.route('/').get(protect, getAllTours).post(protect, createTour)
 
