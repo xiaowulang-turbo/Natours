808aed197b670184a1c3a44a6f075f3872495bba
xiaowu <2728795232@qq.com>
2025-02-03 17:39:00 +0800

    feat: Implement geospatial tours query to find tours within a specified radius

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,7 @@
 const Tour = require('../models/tourModel')
 const APIFeatures = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
-// const AppError = require('../utils/appError')
+const AppError = require('../utils/appError')
 
 const { deleteOne, updateOne, createOne, getOne } = require('./handlerFactory')
 
@@ -125,5 +125,35 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
         status: 'success',
         results: plan.length,
         data: { plan },
+    })
+})
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+    const { distance, latlng, unit } = req.params
+    const [lat, lng] = latlng.split(',')
+
+    const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1
+
+    if (!lat || !lng) {
+        next(
+            new AppError(
+                'Please provide latitude and longitude in the format lat,lng',
+                400
+            )
+        )
+    }
+
+    const tours = await Tour.find({
+        startLocation: {
+            $geoWithin: {
+                $centerSphere: [[lng * 1, lat * 1], radius],
+            },
+        },
+    })
+
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: { tours },
     })
 })
