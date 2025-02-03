792a2fe0c3a586e3f2b10739362547f53dcefd51
xiaowu <2728795232@qq.com>
2025-02-03 21:17:53 +0800

    feat: Add geospatial aggregation to calculate distances from a point

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -157,3 +157,33 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
         data: { tours },
     })
 })
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+    const { latlng, unit } = req.params
+    const [lat, lng] = latlng.split(',')
+
+    // 1 mile = 1609.34 meters
+    const multiplier = unit === 'mi' ? 0.000621371 : 0.001
+
+    const distances = await Tour.aggregate([
+        {
+            // Since we only have one geoSpatial index, it will be used automatically
+            $geoNear: {
+                near: { type: 'Point', coordinates: [lng * 1, lat * 1] },
+                distanceField: 'distance',
+                distanceMultiplier: multiplier,
+            },
+        },
+        {
+            $project: {
+                distance: 1,
+                name: 1,
+            },
+        },
+    ])
+
+    res.status(200).json({
+        status: 'success',
+        data: { distances },
+    })
+})
