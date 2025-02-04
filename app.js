4e58803a173245a455e9a5dc164abd798f25df58
xiaowu <2728795232@qq.com>
2025-02-04 16:01:59 +0800

    feat: Add routes for overview and tour pages

--- app.js
+++ app.js
@@ -95,6 +95,18 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All tours',
+    })
+})
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker',
+    })
+})
+
 // Mounting a new router on a route
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
