4a8fdd60a849027e64d82f7d8f194699d7a80bb6
xiaowu <2728795232@qq.com>
2025-02-04 17:42:32 +0800

    refactor: Integrate view routes into main app routing

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const viewRouter = require('./routes/viewRoutes')
 
 const app = express()
 
@@ -88,24 +89,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Jonas',
-    })
-})
-
-app.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All tours',
-    })
-})
-
-app.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker',
-    })
-})
+app.use('/', viewRouter)
 
 // Mounting a new router on a route
 app.use('/api/v1/tours', tourRouter)
