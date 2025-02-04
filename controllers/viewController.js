0b20467656cb7e581c17c5036c8229b2be526459
xiaowu <2728795232@qq.com>
2025-02-04 18:14:13 +0800

    feat(viewController): add tour data fetching and rendering in overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,22 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
+
+exports.getOverview = catchAsync(async (req, res) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find()
+
+    // 2) Build template
+    // const html = pug.renderFile(`${__dirname}/../views/overview.pug`, {
+    //     title: 'All tours',
+    //     tours,
+    // })
+
+    // 3) Render that template using tour data
     res.status(200).render('overview', {
         title: 'All tours',
+        tours,
     })
-}
+})
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
