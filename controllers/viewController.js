db090a19eb3415f574f66015333a633a498d8576
xiaowu <2728795232@qq.com>
2025-02-04 19:14:45 +0800

    feat(viewController): enhance tour view with error handling and data population

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 exports.getOverview = catchAsync(async (req, res) => {
     // 1) Get tour data from collection
@@ -18,8 +19,21 @@ exports.getOverview = catchAsync(async (req, res) => {
     })
 })
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+    // 1) Get the data, for the requested tour (including reviews and guides)
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user',
     })
-}
+
+    if (!tour) {
+        return next(new AppError('No tour found with that name', 404))
+    }
+
+    // 2) Build template
+    // 3) Render template
+    res.status(200).render('tour', {
+        title: `${tour.name} Tour`,
+        tour,
+    })
+})
