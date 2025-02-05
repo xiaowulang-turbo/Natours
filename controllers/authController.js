7c8571185164ab4dc3b4820e0bdf98f00c4a2346
xiaowu <2728795232@qq.com>
2025-02-05 14:19:48 +0800

    feat(auth): add isLoggedIn middleware to check user session

--- controllers/authController.js
+++ controllers/authController.js
@@ -124,6 +124,29 @@ exports.protect = catchAsync(async (req, res, next) => {
     next()
 })
 
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+    if (req.cookies.jwt) {
+        // 1) Verify token
+        const decoded = await promisify(jwt.verify)(
+            req.cookies.jwt,
+            process.env.JWT_SECRET
+        )
+
+        // 2) Check if user still exists
+        const currentUser = await User.findById(decoded.id)
+        if (!currentUser) return next()
+
+        // 3) Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) return next()
+        // There is a logged user
+        res.locals.user = currentUser
+        return next()
+    }
+    // There is no logged user
+    next()
+})
+
 exports.restrictTo =
     (...roles) =>
     (req, res, next) => {
