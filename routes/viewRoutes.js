86c8cc853a77d55b430128884e500bc1b43f05d2
xiaowu <2728795232@qq.com>
2025-02-05 14:18:38 +0800

    feat(viewRoutes): add isLoggedIn middleware for authentication

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
+const { isLoggedIn } = require('../controllers/authController')
 
 const {
     getOverview,
     getTour,
     getLoginForm,
 } = require('../controllers/viewController')
+
+router.use(isLoggedIn)
 
 router.get('/', getOverview)
 router.get('/tour/:slug', getTour)
