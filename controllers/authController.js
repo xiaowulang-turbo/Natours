505e4105e2d80010ccf66fc5a60908369ee4b8a8
xiaowu <2728795232@qq.com>
2025-02-06 13:15:25 +0800

    fix(authController): enhance logout and isLoggedIn functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,7 +74,7 @@ exports.login = catchAsync(async (req, res, next) => {
 })
 
 exports.logout = (req, res) => {
-    res.cookie('jwt', '', {
+    res.cookie('jwt', 'isLoggedOut', {
         expires: new Date(Date.now() + 10 * 1000),
         httpOnly: true,
     })
@@ -136,25 +136,29 @@ exports.protect = catchAsync(async (req, res, next) => {
 
 // Only for rendered pages, no errors!
 exports.isLoggedIn = async (req, res, next) => {
-    if (req.cookies.jwt) {
-        // 1) Verify token
-        const decoded = await promisify(jwt.verify)(
-            req.cookies.jwt,
-            process.env.JWT_SECRET
-        )
+    try {
+        if (req.cookies.jwt) {
+            // 1) Verify token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            )
 
-        // 2) Check if user still exists
-        const currentUser = await User.findById(decoded.id)
-        if (!currentUser) return next()
+            // 2) Check if user still exists
+            const currentUser = await User.findById(decoded.id)
+            if (!currentUser) return next()
 
-        // 3) Check if user changed password after the token was issued
-        if (currentUser.changedPasswordAfter(decoded.iat)) return next()
-        // There is a logged user
-        res.locals.user = currentUser
-        return next()
+            // 3) Check if user changed password after the token was issued
+            if (currentUser.changedPasswordAfter(decoded.iat)) return next()
+            // There is a logged user
+            res.locals.user = currentUser
+            return next()
+        }
+        // There is no logged user
+        next()
+    } catch (err) {
+        next()
     }
-    // There is no logged user
-    next()
 }
 
 exports.restrictTo =
