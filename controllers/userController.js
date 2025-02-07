f3c05d3b710719a189d6fd6fe7e716db4a55bd14
xiaowu <2728795232@qq.com>
2025-02-07 17:46:41 +0800

    feat(userController): enhance user photo upload with custom storage and filter

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,10 +19,30 @@ const filterObj = (obj, ...allowedFields) => {
     return newObj
 }
 
-exports.uploadUserPhoto = multer({
-    dest: 'public/img/users',
-    // storage: multer.memoryStorage(),
-}).single('photo')
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/users')
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split('/')[1]
+        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+    },
+})
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true)
+    } else {
+        cb(new AppError('Not an image! Please upload only images.', 400), false)
+    }
+}
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+})
+
+exports.uploadUserPhoto = upload.single('photo')
 
 exports.getAllUsers = getAll(User)
 
