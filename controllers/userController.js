c348e317fd0f5d71c19b15b0921328699666592c
xiaowu <2728795232@qq.com>
2025-02-07 22:03:14 +0800

    feat(userController): add image resizing with sharp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const multer = require('multer')
+const sharp = require('sharp')
 
 const User = require('../models/userModel')
 const AppError = require('../utils/appError')
@@ -19,15 +20,16 @@ const filterObj = (obj, ...allowedFields) => {
     return newObj
 }
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img/users')
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split('/')[1]
-        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
-    },
-})
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users')
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split('/')[1]
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`)
+//     },
+// })
+const multerStorage = multer.memoryStorage()
 
 const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -44,12 +46,21 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo')
 
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+    if (!req.file) return next()
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+    await sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${req.file.filename}`)
+    next()
+})
+
 exports.getAllUsers = getAll(User)
 
 // updateMe
 exports.updateMe = catchAsync(async (req, res, next) => {
-    console.log('req.file', req.file)
-    console.log('req.body', req.body)
     // 1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
         return next(
