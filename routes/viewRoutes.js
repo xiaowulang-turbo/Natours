257f0a8b9ab748d8349ec93bb6bfa37c2d29b258
xiaowu <2728795232@qq.com>
2025-02-07 15:49:02 +0800

    refactor(viewRoutes): comment out updateUserData route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,7 +9,7 @@ const {
     getTour,
     getLoginForm,
     getAccount,
-    updateUserData,
+    // updateUserData,
 } = require('../controllers/viewController')
 
 // router.use(isLoggedIn)
@@ -18,5 +18,7 @@ router.get('/', isLoggedIn, getOverview)
 router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, getLoginForm)
 router.get('/me', protect, getAccount)
-router.post('/submit-user-data', protect, updateUserData)
+// router.post('/submit-user-data', protect, updateUserData)
+// router.post('/submit-user-data', protect, updateUserData)
+
 module.exports = router
