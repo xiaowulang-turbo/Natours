818edd9234a6233f138953b84a1948bdfe9bfc48
xiaowu <2728795232@qq.com>
2025-02-07 15:48:37 +0800

    feat: add user data and password update functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 import '@babel/polyfill'
 import { login, logout } from './login'
 import displayMap from './mapbox'
-
+import { updateSettings } from './updateSettings'
 // DOM ELEMENTS
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form.form--login')
 const logoutBtn = document.querySelector('.nav__el--logout')
+const userDataForm = document.querySelector('.form.form-user-data')
+const userPasswordForm = document.querySelector('.form.form-user-settings')
 
 // DElEGATION
 if (mapBox) {
@@ -27,4 +29,13 @@ if (loginForm) {
 
 if (logoutBtn) {
     logoutBtn.addEventListener('click', logout)
+}
+
+if (userDataForm) {
+    userDataForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const name = document.getElementById('name').value
+        const email = document.getElementById('email').value
+        updateSettings({ name, email }, 'data')
+    })
 }
