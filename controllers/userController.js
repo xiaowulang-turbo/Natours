16c052abbb13acda34606125d67c2383152f83cc
xiaowu <2728795232@qq.com>
2025-02-07 15:48:31 +0800

    feat(userController): add console log for updateMe request body

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,7 @@ exports.getAllUsers = getAll(User)
 
 // updateMe
 exports.updateMe = catchAsync(async (req, res, next) => {
+    console.log('req.body', req.body)
     // 1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
         return next(
